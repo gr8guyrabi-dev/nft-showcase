1b72fdcc5a42c2361a50ad043d9f13b0c5369f9a
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-05-05 16:10:36 +0545

    setup sectiowrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Download, Features, SectionWrapper } from './components'
+import assets from './assets'
 
 const App = () => {
     return (
         <>
-            <Download />
-            <Features />
-            <SectionWrapper />
+            <SectionWrapper 
+                title="You own store of Nifty NFTs. Start Selling & Growing"
+                description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+                showBtn
+                mockupImg={assets.homeHero}
+                banner="banner"
+            />
         </>
     );
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const SectionWrapper = () => {
-  return (
-    <div>SectionWrapper</div>
-  )
+import Button from './Button'
+import styles from '../styles/Global'
+import assets from '../assets'
+
+const SectionWrapper = ({title, description, showBtn, mockupImg, banner}) => {
+    return (
+        <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
+            <div className={`flex items-center ${styles.boxClass} w-11/12 sm:w-full minmd:w-3/4`}>
+                <div className={`${styles.descDiv} fadeLeftMini`}>
+                    <h1 className={`${styles.h1Text}`}>{title}</h1>
+                    <p className={`${styles.descriptionText}`}>{description}</p>
+                    { showBtn && (
+                        <Button 
+                            assetUrl={assets.expo}
+                            link="marketplace"
+                        />
+                    )}
+                </div>
+                <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
+                    <img src={mockupImg} alt="mockup" className={styles.sectionImg}/>
+                </div>
+            </div>
+            
+            
+            
+            
+        </div>
+    )
 }
 
 export default SectionWrapper
